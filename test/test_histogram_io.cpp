c7ca544f92ba6f3c6a7e9fddc261c3a802c90ea0
Ko-Chih Wang <caseywang777@gmail.com>
2016-08-04 14:28:51 -0400

    update new test_histogram_io example

--- test/test_histogram_io.cpp
+++ test/test_histogram_io.cpp
@@ -11,47 +11,99 @@ using namespace edda;
 using namespace std;
 using namespace edda::dist;
 
+void loadFloatRawData(char* path, int D0, int D1, int D2, float* data){
+	FILE* fp = fopen(path, "rb");
+	fread(data, sizeof(float), D0*D1*D2, fp);
+	fclose(fp);
+}
+
+AbstractDistrArray* computeDistributionArrayFromBlocks(float* data, int blockSize, int dsD0, int dsD1, int dsD2, int D0, int D1, int D2){
+	int nblock = dsD0*dsD1*dsD2;
+	float *dataPoint = (float*)malloc(sizeof(float)*blockSize*blockSize*blockSize);
+	shared_ary<Histogram> array(new Histogram[nblock], nblock);
+	int blkCnt = 0;
+	for (int blkD0 = 0; blkD0 < dsD0; blkD0++){
+		int d0s = blkD0 * blockSize;
+		int d0t = d0s + blockSize;
+		if (d0t > D0) d0t = D0;
+		for (int blkD1 = 0; blkD1 < dsD1; blkD1++){
+			int d1s = blkD1 * blockSize;
+			int d1t = d1s + blockSize;
+			if (d1t > D1) d1t = D1;
+			for (int blkD2 = 0; blkD2 < dsD2; blkD2++){
+				int d2s = blkD2 * blockSize;
+				int d2t = d2s + blockSize;
+				if (d2t > D2) d2t = D2;
+				int cnt = 0;
+
+				for (int d0 = d0s; d0 < d0t; d0++){
+					for (int d1 = d1s; d1 < d1t; d1++){
+						for (int d2 = d2s; d2 < d2t; d2++){
+							dataPoint[cnt++] = data[d0*D1 * D2 + d1*D2 + d2];
+						}
+					}
+				}
+				array[blkCnt++] = eddaComputeHistogram(dataPoint, cnt, 128, -1000, 1000);
+				//array[blkCnt++] = eddaComputeHistogram(dataPoint, cnt, 128);
+			}
+		}
+	}
+	return new DistrArray<Histogram>(array);
+}
+
+void writeResampleData(shared_ptr<Dataset<Real> > dataset2, int dsD0, int dsD1, int dsD2, int upsample)
+{
+	int* dim;
+	dim = dataset2->getDimension();
+
+	FILE* ofp = fopen("histOutput.raw", "wb");
+	float tmp[1];
+	float step = 1.0 / (float)upsample;
+	for (float k = 0; k < dim[2]; k += step){
+		printf("%f\n", k);
+		for (float j = 0; j < dim[1]; j += step){
+			for (float i = 0; i < dim[0]; i += step){
+				VECTOR3 pos = VECTOR3(i, j, k);
+				float v;
+				dataset2->at_phys(pos, v);
+				tmp[0] = v;
+				fwrite(tmp, sizeof(float), 1, ofp);
+			}
+		}
+	}
+	fclose(ofp);
+	printf("Dimenstion: %d %d %d\n", dim[0] * upsample, dim[1] * upsample, dim[2] * upsample);
+}
 
 int main()
 {
-	shared_ary<Histogram> array (new Histogram[8], 8);
-	for (int i = 0; i < 8; i++){
-		float data[1];
-		data[0] = 50 + i;
-		//array[i] = Histogram(data, 1, 50, 60, 20);
-		//array[i] = eddaComputeHistogram(data, 1, 50, 60, 20);
-		array[i] = eddaComputeHistogram(data, 1, 20);
-	}
-	AbstractDistrArray * abstract_array = new DistrArray<Histogram>(array);
+	int blockSize = 8;	//local block size
+	int D0 = 100, D1 = 500, D2 = 500;	//raw data dimenstion
+	char path[500] = "C:\\GravityLabDataSet\\IsabelMultiVariable\\Pf07.raw";	//raw data path
+	int dsD0 = int(ceil(D0 / float(blockSize)));	//resolution of blocks
+	int dsD1 = int(ceil(D1 / float(blockSize)));
+	int dsD2 = int(ceil(D2 / float(blockSize)));
 
-	for (int i = 0; i<8; i++)
-	{
-		printf("%d\n", i);
-		cout << i << ": " << abstract_array->getScalar(i) <<
-			": sample = " << getSample(abstract_array->getScalar(i)) << endl;
-	}
+	//load raw data set
+	float* gtDetData = (float*)malloc(sizeof(float)*D0*D1*D2);
+	loadFloatRawData(path, D0, D1, D2, gtDetData);
 
+	//create distribution array from blocks
+	AbstractDistrArray * abstract_array = computeDistributionArrayFromBlocks(gtDetData, blockSize, dsD0, dsD1, dsD2, D0, D1, D2);
+
+	//writer: write distribution array to disk
 	shared_ptr<Dataset<Real> > dataset = make_Dataset<Real>(
-		new RegularCartesianGrid(2,2,2),
+		new RegularCartesianGrid(dsD2, dsD1, dsD0),
 		abstract_array
-	);
+		);
 	writeEddaVtkDataset(dataset, "testHist.vti", "test_");
-	cout << "finish histogram modeling and write into file, press any key to continue!" << endl;
-	//getchar();
-																															
-																																
+
+	//reader: read distribution array from disk
 	shared_ptr<Dataset<Real> > dataset2 = loadEddaScalarDataset("testHist.vti", "test_");
-	cout << dataset2->getArray()->getDistrName() << endl;
-	int* dim;
-	dim = dataset2->getDimension();
-	cout << dim[0] << dim[1] << dim[2]  << endl;
 
-	for (int i = 0; i < dim[0]; i++)
-		for (int j = 0; j < dim[1]; j++)
-			for (int k = 0; k < dim[2]; k++)
-				cout << "at_comp(" <<i << "," << j << "," << k << ") : " << dataset2->at_comp(i, j, k) << endl;
-  cout << "load histogram from file and sampling, press any key to finish!" << endl;
-  //getchar();
-  return 0;
+	//sample and output raw data to disk
+	writeResampleData(dataset2, dsD0, dsD1, dsD2, 8);
+
+	cout << "Done, press any key to finish!" << endl;
+	getchar();
 }
-
